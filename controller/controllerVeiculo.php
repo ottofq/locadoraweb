845da55066c1a2f1dc5364e19a3dfedea8e3234d
Otto Freitas <ottofq@gmail.com>
2019-05-10 20:39:24 -0300

    Add controller veiculo

---
+++ controller/controllerVeiculo.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once '../model/veiculo.php';
+require_once '../dao/veiculoDAO.php';
+
+$opcao = (int) $_REQUEST['opcao'];
+
+if ($opcao === 1) {
+    $veiculo    = new Veiculo($_REQUEST['txtPlacaVeiculo'], $_REQUEST['txtNomeVeiculo'], $_REQUEST['txtAnoVeiculo'], $_REQUEST['txtFabricanteVeiculo'], $_REQUEST['txtOpcionaisVeiculo'], $_REQUEST['txtMotorizacaoVeiculo'], $_REQUEST['txtValorVeiculo'], $_REQUEST['txtCategoriaVeiculo']);
+    $veiculoDAO = new VeiculoDAO();
+
+    $veiculoDAO->incluirVeiculo($veiculo);
+
+    header("Location:controllerVeiculo.php?opcao=2");
+}
