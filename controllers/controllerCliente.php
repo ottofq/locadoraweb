4c3e3a0ae841b45c4c739ea2bb3265802eead58c
Otto Freitas <ottofq@gmail.com>
2019-05-31 12:59:25 -0300

    add controller cliente

---
+++ controllers/controllerCliente.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once '../model/cliente.php';
+require_once '../dao/clienteDAO.php';
+
+$opcao = (int) $_REQUEST['opcao'];
+
+if ($opcao === 1) {
+    $cliente    = new Cliente($_REQUEST['txtCPFCliente'], $_REQUEST['txtNomeCliente'], $_REQUEST['txtRGCliente'], $_REQUEST['txtEnderecoCliente'], $_REQUEST['txtTelefoneCliente'], $_REQUEST['txtEmailCliente']);
+    $clienteDAO = new ClienteDAO();
+
+    $ClienteDAO->incluirCliente($cliente);
+
+    //header("Location:controllerCliente.php?opcao=2");
+}
