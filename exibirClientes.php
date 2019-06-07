0fed99120ce1e5f8f82067dbad58379c9b6e3afc
Miquéas Teixeira <miqueas.at@gmail.com>
2019-06-07 10:53:37 -0300

    Add icones das acoes alterar e excluir

--- exibirClientes.php
+++ exibirClientes.php
@@ -29,13 +29,19 @@ $clientes = $_SESSION['ListaClientes'];
             <td><?php echo $cliente->endereco ?></td>
             <td><?php echo $cliente->telefone ?></td>
             <td><?php echo $cliente->email ?></td>
-            <td class="btn-exibirClientes">
+            <td>
                 <a href="controllers/controllerCliente.php?opcao=3&cpf=<?php echo $cliente->cpf ?>" >
-                    <button class="btn btn-outline-success">Editar</button>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="16" viewBox="0 0 14 16">
+                      <title>Alterar</title>
+                      <path fill-rule="evenodd" d="M0 12v3h3l8-8-3-3-8 8zm3 2H1v-2h1v1h1v1zm10.3-9.3L12 6 9 3l1.3-1.3a.996.996 0 0 1 1.41 0l1.59 1.59c.39.39.39 1.02 0 1.41z"/>
+                    </svg>
                 </a>
-
-                <a  href="controllers/controllerCliente.php?opcao=4&cpf=<?php echo $cliente->cpf ?>" >
-                    <button class="btn btn-outline-success">Excluir</button>
+                &nbsp&nbsp&nbsp
+                <a  href="controllers/controllerCliente.php?opcao=4&cpf=<?php echo $cliente->cpf ?>">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="12" height="16" viewBox="0 0 12 16">
+                      <title>Excluir</title>
+                      <path fill-rule="evenodd" d="M11 2H9c0-.55-.45-1-1-1H5c-.55 0-1 .45-1 1H2c-.55 0-1 .45-1 1v1c0 .55.45 1 1 1v9c0 .55.45 1 1 1h7c.55 0 1-.45 1-1V5c.55 0 1-.45 1-1V3c0-.55-.45-1-1-1zm-1 12H3V5h1v8h1V5h1v8h1V5h1v8h1V5h1v9zm1-10H2V3h9v1z"/>
+                    </svg>
                 </a>
             </td>
         </tr>
