26fdd7ca9696f84e321faa90813807f572954bfb
romersonmedina <romersonmn@gmail.com>
2019-07-04 13:16:50 -0300

    pagina fale conosco

---
+++ faleConosco.php
@@ -0,0 +1,65 @@
+<?php include_once 'cabecalho.php'?>
+
+    <div class="container">
+        <h2>Entre em contato conosco!</h2>
+        <p>Aqui você irá encontrar o carro que precisa para sua viagem ou passeio.
+           Entre em contato conosco ou faça-nos uma visita.</p>
+    </div>
+  <br>
+  <br>
+  <br>
+<div class="container">
+  <div class="row">
+    <div class="col-md-3" style="border:solid black 1px; padding:2px;"><img src="imagens/telefone.svg" width="30px" height="30px"> Reserve pelo telefone<br>
+      <br>
+      <p>3330-2019</p>
+      <p>+55 28 2101-8080</p>
+      <p><font size="2" color="green">Capitais e regiões metropolitanas</font></p>
+      <p><b>__________________________</b></p>
+      <p>3331-2019</p>
+      <p>+55 28 2101-8081</p>
+      <p><font size="2" color="green">Demais localidades</font></p>
+      <p><b>__________________________</b></p>
+      <p><font size="2" color="green"><b>Horário de atendimento</b></font></p>
+      <p><font size="2">Segunda a sexta de 08h às 22h</font></p>
+      <p><font size="2" >Sábado de 09h às 20h</font></p>
+      <p><font size="2" >Domingo e feriados de 10h às 19h</font></p>
+  </div>
+    <div class="col-md-3" style="border:solid black 1px; padding:2px;"><img src="imagens/posvenda.svg" width="30px" height="30px"> Pós-vendas<br>
+      <br>
+      <p><font size="2">Para locação através do site ou telefone</font></p>
+      <p>+55 28 2101-8080</p>
+      <p><font size="2"  color="green">Capitais e regiões metropolitanas</font></p>
+      <p><b>__________________________</b></p>
+      <p>3331-2019</p>
+      <p>+55 28 2101-8081</p>
+      <p><font size="2"  color="green">Demais localidades</font></p>
+      <p><b>__________________________</b></p>
+      <p><font size="2"  color="green"><b>Horário de atendimento</b></font></p>
+      <p><font size="2" >Segunda a sexta de 09h às 20:00h</font></p>
+      <p><font size="2" >Sábado de 09h às 18h</font></p>
+    </div>
+    <div class="col-md-3" style="border:solid black 1px; padding:2px;"><img src="imagens/fone.svg" width="30px" height="30px"> SAC
+      <p><font size="2" ><b>Serviço de atendimento ao cliente</b></font></p>
+      <p><font size="2" >Para críticas e elogios</font></p>
+      <p>+55 28 2101-8088</p>
+      <p><font size="2"  color="green">Capitais e regiões metropolitanas</font></p>
+      <p><b>__________________________</b></p>
+      <p>0300-721-2166</p>
+      <p><font size="2"  color="green">Demais localidades</font></p>
+      <p><b>__________________________</b></p>
+      <p><font size="2"  color="green"><b>Horário de atendimento</b></font></p>
+      <p><font size="2" >Segunda a sexta de 09h às 20:00h</font></p>
+      <p><font size="2" >Sábado de 09h às 18h</font></p>
+    </div>
+    <div class="col-md-3" style="border:solid black 1px; padding:2px;"><img src="imagens/email.svg" width="30px" height="30px"> Fale Conosco<br>
+      <br>
+      <p><font size="2" >Qualquer dúvida, informação ou sugestão</font></p>
+      <br>
+      <p><font size="4" >Mande um email para <b>alegrecar@gmail.com</b></font></p>
+      <p><b>__________________________</b></p>
+    </div>
+  </div>
+</div>
+
+<?php include_once 'rodape.php'?>
